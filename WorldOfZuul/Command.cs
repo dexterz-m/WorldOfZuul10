de70a5f3211b2e95c82e5003b39b8c96dc6a77c5
Zolizoli110 <zolika.suveges@gmail.com>
2025-10-22 14:03:36 +0200

    Command class extended to 2 sub commands

--- WorldOfZuul/Command.cs
+++ WorldOfZuul/Command.cs
@@ -10,11 +10,13 @@ namespace WorldOfZuul
     {
         public string Name { get; }
         public string? SecondWord { get; } // this might be used for future expansions, such as "take apple".
+        public string?  ThirdWord { get; }
 
-        public Command(string name, string? secondWord = null)
+        public Command(string name, string? secondWord = null,  string? thirdWord = null)
         {
             Name = name;
             SecondWord = secondWord;
+            ThirdWord = thirdWord;
         }
     }
 }
