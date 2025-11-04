3c965af178189b2c36e6e35e3a776852c99956bc
Zolizoli110 <zolika.suveges@gmail.com>
2025-11-04 11:34:43 +0100

    assign villagers to jobs still needs refining

---
+++ WorldOfZuul/Jobs/Lumberjack.cs
@@ -0,0 +1,18 @@
+﻿namespace WorldOfZuul.Jobs;
+
+public class Lumberjack : Job
+{
+    private int _resourceGainedPerTurn;
+    private int _treesCutDownPerTurn;
+
+    public Lumberjack(int resourceGainedPerTurn, int treesCutDownPerTurn) : base(1, "Lumberjack", "")
+    {
+        _resourceGainedPerTurn = resourceGainedPerTurn;
+        _treesCutDownPerTurn = treesCutDownPerTurn;
+    }
+
+    public override void Work()
+    {
+        //TODO: Implement cutting trees
+    }
+}
---
+++ WorldOfZuul/Jobs/Unemployed.cs
@@ -0,0 +1,9 @@
+﻿namespace WorldOfZuul.Jobs;
+
+public class Unemployed : Job
+{
+    public Unemployed() : base(0, "Unemployed", "")
+    {
+        
+    }
+}
