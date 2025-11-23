0a58f9e2472a7be99238772f9d6dd24f71c30971
Zolizoli110 <zolika.suveges@gmail.com>
2025-11-23 21:09:15 +0100

    Refactored to auto properties

--- WorldOfZuul/Resources.cs
+++ WorldOfZuul/Resources.cs
@@ -2,66 +2,12 @@ namespace WorldOfZuul;
 
 public class Resources
 {
+    public int Food { get; set; } = 10;
+    public int GrainSeeds { get; set; } = 2;
+    public int Grains { get; set; } = 0;
 
-    public int Food
-    {
-        get => _food;
-        set => _food += value;
-    }
-
-    public int GrainSeeds
-    {
-        get => _grainSeeds;
-        set => _grainSeeds += value;
-    }
-
-    public int Grains
-    {
-        get => _grains;
-        set => _grains += value;
-    }
-
-    public int Hunger
-    {
-        get => _hunger;
-        set => _hunger += value;
-    }
-
-    public int Animals
-    {
-        get => _animals;
-        set => _animals += value;
-    }
-
-    public int Trees
-    {
-        get => _trees;
-        set => _trees += value;
-    }
-
-    public int Wood
-    {
-        get => _wood;
-        set => _wood += value;
-    }
-
-    public int Saplings
-    {
-        get => _saplings;
-        set => _saplings += value;
-    }
-
-    // Food and farming variables
-    private int _food = 10;
-    private int _grainSeeds = 2;
-    private int _grains = 0;
-    private int _hunger = 0; // This can be assigned as 100 in start as 100%, but everyday it reduces by 25-40%, so player have to feed villagers everyday.
-
-    // animals and forest variables 
-    private int _animals = 100;
-    private int _trees = 100;
-    private int _wood = 0;
-    private int _saplings = 0;
-    
-    
+    public int Animals { get; set; } = 100;
+    public int Trees { get; set; } = 100;
+    public int Wood { get; set; } = 0;
+    public int Saplings { get; set; } = 0;
 }
